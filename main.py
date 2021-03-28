2915000083f81094d8887d1b48a7786a929dec04
PanidaOun <panida.ou@ku.th>
2021-03-29 00:06:43 +0700

    Add different image for the two Pacman's Resolve #5

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class SuperPacmanState:
             self.pacman.y += 2 * PACMAN_SPEED * DIR_OFFSET[self.pacman.direction][1]
     
 
-class Pacman(Sprite):
+class Pacman1(Sprite):
     def __init__(self, app, maze, r, c):
         self.r = r
         self.c = c
@@ -81,13 +81,46 @@ class Pacman(Sprite):
     def set_next_direction(self, direction):
         self.next_direction = direction
 
+class Pacman2(Sprite):
+    def __init__(self, app, maze, r, c):
+        self.r = r
+        self.c = c
+        self.maze = maze
+
+        self.direction = DIR_STILL
+        self.next_direction = DIR_STILL
+
+        self.dot_eaten_observers = []
+
+        x, y = maze.piece_center(r,c)
+        super().__init__(app, 'images/pacman_2.png', x, y)
+        self.state = NormalPacmanState(self)
+
+    def update(self):
+        if self.maze.is_at_center(self.x, self.y):
+            r, c = self.maze.xy_to_rc(self.x, self.y)
+
+            if self.maze.has_dot_at(r, c):
+                self.maze.eat_dot_at(r, c)
+                for callObservers in self.dot_eaten_observers:
+                    callObservers()
+            
+                self.state.random_upgrade()
+            if self.maze.is_movable_direction(r, c, self.next_direction):
+                self.direction = self.next_direction
+            else:
+                self.direction = DIR_STILL
+        self.state.move_pacman()
+
+    def set_next_direction(self, direction):
+        self.next_direction = direction
 
 class PacmanGame(GameApp):
     def init_game(self):
         self.maze = Maze(self, CANVAS_WIDTH, CANVAS_HEIGHT)
 
-        self.pacman1 = Pacman(self, self.maze, 1, 1)
-        self.pacman2 = Pacman(self, self.maze, self.maze.get_height() - 2, self.maze.get_width() - 2)
+        self.pacman1 = Pacman1(self, self.maze, 1, 1)
+        self.pacman2 = Pacman2(self, self.maze, self.maze.get_height() - 2, self.maze.get_width() - 2)
 
         self.pacman1_score_text = Text(self, 'P1: 0', 100, 20)
         self.pacman2_score_text = Text(self, 'P2: 0', 600, 20)
